d2f4cfa3965d2e44b367cd31e198b8f22c5e42d9
MarceloSaito <mn_saito@hotmail.com>
2021-05-12 09:26:08 -0300

    codigo pronto

--- raspagem_discursos.R
+++ raspagem_discursos.R
@@ -8,6 +8,7 @@ library(httr)
 library(jsonlite)
 library(pdftools)
 library(magrittr)
+library(stringr)
 
 # raspagem discursos oficiais ---------------------------------------------
 
@@ -17,70 +18,118 @@ u_base <- "https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/discursos"
 
 r <- httr::GET(u_base)
 
-discursos_links <- r %>%
-        xml2::read_html() %>% 
-        xml2::xml_find_all("//*[@class = 'summary url']") %>% 
-        xml2::xml_attr("href") %>% 
-        xml2::url_absolute("https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/discursos")
-
 raspador <- function(.x, p){
         p()
-        pagina_discurso <- xml2::read_html(.x)  #acho q aqui é o problema
+        pagina_discurso <- xml2::read_html(.x)  
         discurso <- pagina_discurso %>%
                 xml2::xml_find_all("//p") %>%
                 xml2::xml_text() %>%
                 paste(collapse = " ")
 }
 
-discursos_1_pag <- progressr::with_progress({
-        p <- progressr::progressor(length(discursos_links))
-        discursos <- sapply(discursos_links, raspador, USE.NAMES = FALSE, p)
-})
+discursos_df <- data.frame()
 
-nomes_discursos <- r %>% 
-        xml2::read_html() %>% 
-        xml2::xml_find_all("//*[@title = 'Document']") %>% 
-        xml2::xml_text()
-
-datas_discursos <- r %>% 
-        xml2::read_html() %>% 
-        xml2::xml_find_all("//i[@class = 'icon-day']") %>% 
-        xml2::xml_text()
+for (pagina_url in seq(0,360,30)){
         
+        link <- paste0('https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/discursos?b_start:int=',
+        pagina_url)
         
+        pagina_especifica <- httr::GET(link)
         
+        discursos_links <- pagina_especifica %>%
+                xml2::read_html() %>% 
+                xml2::xml_find_all("//*[@class = 'summary url']") %>% 
+                xml2::xml_attr("href") %>% 
+                xml2::url_absolute("https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/discursos")
         
-discursos_df <- data.table::data.table(nomes_discursos, discursos_1_pag)
-
+        nomes_discursos <- pagina_especifica %>% 
+                xml2::read_html() %>% 
+                xml2::xml_find_all("//*[@title = 'Document']") %>% 
+                xml2::xml_text()
+        
+        datas_discursos <- pagina_especifica %>% 
+                xml2::read_html() %>% 
+                xml2::xml_find_all("//span[@class = 'summary-view-icon']") %>% 
+                xml2::xml_text() %>% 
+                .[seq(1,90,3)] %>% 
+                str_extract_all("\\d+/\\d+/\\d+") %>% 
+                lubridate::dmy()
+        
+        textos_discursos <- progressr::with_progress({
+                p <- progressr::progressor(length(discursos_links))
+                discursos <- sapply(discursos_links, raspador, USE.NAMES = FALSE, p)
+        })
+        
+        discursos_df <- rbind(discursos_df, dplyr::data_frame(nomes_discursos,
+                                                                datas_discursos,
+                                                                textos_discursos)
+        )
+        
+        print(paste("Calma q já baixamo mais de", pagina_url))
+        
+}
 
 
         
 # testes e erros ----------------------------------------------------------
 
 # #teste uma pagina de discurso
-# 
+#
 # discurso_1 <- "https://www.gov.br/planalto/pt-br/acompanhe-o-planalto/discursos/discurso-do-presidente-da-republica-jair-bolsonaro-na-cerimonia-alusiva-a-liberacao-de-trafego-na-ponte-sobre-o-rio-madeira-na-br-364-no-distrito-de-abuna-porto-velho-ro"
-# 
+#
 # texto_1 <- discurso_1 %>%
 #         xml2::read_html() %>%
 #         xml2::xml_find_all("//p") %>%
 #         xml2::xml_text() %>%
-#         paste(collapse = " ")
+        # paste(collapse = " ")
 
-## ta dando erro 
+
+
+
+
+
+# ta dando erro
 # raspar_discursos <- function(link_discurso){
 #         pagina_discurso <- xml2::read_html(link_discurso)
-#         discurso <- pagina_discurso %>% 
-#                 xml2::read_html() %>% 
-#                 xml2::xml_find_all("//p") %>% 
-#                 xml2::xml_text() %>% 
-#                 paste()    
+#         discurso <- pagina_discurso %>%
+#                 xml2::read_html() %>%
+#                 xml2::xml_find_all("//p") %>%
+#                 xml2::xml_text() %>%
+#                 paste()
 # }
+
+
+
+
+
 
 # discursos <- sapply(discursos_links, FUN = get_discursos, USE.NAMES = F)
 
-## assim consigo ler todos os links
+
+
+
+
+
+
+# assim consigo ler todos os links
 # discursos_links %>% purrr::map(read_html)
+
+
+
+
+#datas leticia
+
+# datas_discursos <- datas_discursos[datas_discursos !="\n                        \n                        Página\n                    " & datas_discursos %like% "/"]
+#         
+# datas_discursos <- substr(datas_discursos, 51, 60)   
+# 
+# datas_discursos <- as.Date(datas_discursos)
+# 
+# datas_discursos <- as.Date(datas_discursos, tryFormats = "%d/%m/%Y")
+# 
+# datas_discursos <- format(datas_discursos, "%d/%m/%Y")
+# 
+
 
 
 
